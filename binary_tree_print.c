da70bafc25cf42ec4c74697e42af6b40af9658a0
David Muindi <muindidiego@gmail.com>
2023-06-01 10:37:27 -0700

    Task 0

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -2,17 +2,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_print - Prints a binary tree using ASCII art
- * @tree: Pointer to the root node of the tree to print
+ * binary_tree_print_helper - Helper function to print a binary tree
+ * @tree: Pointer to the current node
+ * @prefix: Prefix string for indentation
+ * @is_left: Flag indicating if the current node is the left child
  */
-void binary_tree_print(const binary_tree_t *tree)
+static void binary_tree_print_helper(const binary_tree_t *tree,
+		const char *prefix, int is_left)
 {
 	if (tree == NULL)
 		return;
 
+	printf("%s", prefix);
+	printf(is_left ? "├──" : "└──");
 	printf("(%d)\n", tree->n);
-	printf(".--");
-	binary_tree_print(tree->left);
-	printf("   .--");
-	binary_tree_print(tree->right);
+
+	binary_tree_print_helper(tree->left, prefix, 1);
+	binary_tree_print_helper(tree->right, prefix, 0);
+}
+
+/**
+ * binary_tree_print - Prints a binary tree
+ * @tree: Pointer to the root node of the tree to print
+ */
+void binary_tree_print(const binary_tree_t *tree)
+{
+	binary_tree_print_helper(tree, "", 0);
 }
